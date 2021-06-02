5f31496a743f4584a20c976c33e738f7705c4608
slycoolgamer <62766294+slycoolgamer@users.noreply.github.com>
2021-06-02 19:40:00 +0100

    Add files via upload

---
+++ Unop coin/EconomyBot-main/botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "ODQ5Njk4NTA1NzI3ODAzMzk0.YLe9Zg.luvdvd-Flvb7WYFIb85rQ61sGEA",
+    prefix: "/",
+    admins: [
+        "617331258933903390"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
